c374880b429cd0c3b0eadd60b4c201f52b7f3ee8
Develop <ram2055@mail.ru>
2021-01-08 16:52:42 +0500

    Initial commit

---
+++ src/main/java/org/coinventory/MainController.java
@@ -0,0 +1,12 @@
+package org.coinventory;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping(value = "/")
+    public String index() {
+        return "";
+    }
+}
