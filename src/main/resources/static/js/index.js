c61cbb8bf01a368b79d5d103926ffd066a64df85
develop <ram2055@mail.ru>
2021-02-14 13:33:51 +0500

    Added reactjs and bootstrap

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+    render() {
+        return (
+            <b>Main page</b>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+    )
