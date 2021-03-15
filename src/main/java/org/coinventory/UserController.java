7f2795fddce839aa94b5489331d7a328ea548418
develop <ram2055@mail.ru>
2021-03-15 21:23:54 +0500

    Implemented post method for /user endpoint

--- src/main/java/org/coinventory/UserController.java
+++ src/main/java/org/coinventory/UserController.java
@@ -1,10 +1,40 @@
 package org.coinventory;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.coinventory.db.LoginUsers;
+import org.coinventory.db.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService us;
+    @Autowired
+    UserAndLoginService uls;
+
+    @PostMapping()
+    public ResponseEntity<?> createUser(@RequestBody Map<String,String> params) {
+        Users u = new Users();
+        u.setUname(params.get("uname"));
+        u.setSurname(params.get("surname"));
+        String email = params.getOrDefault("email", "");
+        String role = params.getOrDefault("role", "");
+        String password = params.getOrDefault("password", "");
+        if (!email.isEmpty() && !role.isEmpty() && !password.isEmpty()) {
+            LoginUsers lu = new LoginUsers();
+            lu.setEmail(email);
+            lu.setPwd(password);
+            lu.setAdm(role.equals("administrator"));
+            uls.insertUserAndLogin(u, lu);
+        }
+        else {
+            us.insertUser(u);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
 }
