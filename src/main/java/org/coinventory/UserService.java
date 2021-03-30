db5f3b39214aa57f0089a91e11872eaeca6270f5
develop <ram2055@mail.ru>
2021-03-30 20:35:12 +0500

    Find user by surname and find all users

--- src/main/java/org/coinventory/UserService.java
+++ src/main/java/org/coinventory/UserService.java
@@ -1,10 +1,15 @@
 package org.coinventory;
 
 import org.coinventory.db.Users;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface UserService {
     Users insertUser(Users u);
     Users updateUser(Users u);
     void deleteUser(long id);
     Users getUser(long id);
+    List<Users> getUsersBySurname(String surname, Pageable p);
+    List<Users> getUsersAll();
 }
--- src/main/java/org/coinventory/UserServiceImpl.java
+++ src/main/java/org/coinventory/UserServiceImpl.java
@@ -3,8 +3,11 @@ package org.coinventory;
 import org.coinventory.db.Users;
 import org.coinventory.db.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -30,5 +33,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public Users getUser(long id) {
         return null;
+    }
+
+    @Override
+    public List<Users> getUsersBySurname(String surname, Pageable p) {
+        return ur.findBySurnameContainingIgnoreCase(surname, p);
+    }
+
+    public List<Users> getUsersAll() {
+        return ur.findAll();
     }
 }
