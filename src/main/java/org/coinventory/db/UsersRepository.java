bc62e20627de4b50a5681d8caf130509a3ddf04f
develop <ram2055@mail.ru>
2021-03-30 20:30:39 +0500

    Use JPARepository instead CrudRepository

--- src/main/java/org/coinventory/db/UsersRepository.java
+++ src/main/java/org/coinventory/db/UsersRepository.java
@@ -1,10 +1,11 @@
 package org.coinventory.db;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+import java.util.List;
 
 @Repository
-public interface UsersRepository extends CrudRepository<Users, Long> {
-    Optional<Users> findBySurname(String surname);
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    List<Users> findBySurnameContainingIgnoreCase(String surname, Pageable p);
 }
